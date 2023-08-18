f85093b8b67ba2b96cf17462e7153675475829c2
29534411212 <tkalyoncuoglu@gmail.com>
2023-08-18 18:51:50 +0300

    second commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from 'react-dom/client';
+
+import App from "./components/App";
+import './css/main.scss';
+
+
+ReactDOM.render(<App/>, document.getElementById("root"));
+
+
+
+
+
+
+
+
